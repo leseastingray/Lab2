38918cc6cf4857da85c868e2bfd97ca6d21f8537
Megan Bonebrake <bonebrakeb@my.lanecc.edu>
2019-02-04 13:49:22 -0800

    Tutorial 8-1 completed

--- Tutorial 8-1/Password Validation/Password Validation/Form1.cs
+++ Tutorial 8-1/Password Validation/Password Validation/Form1.cs
@@ -26,14 +26,16 @@ namespace Password_Validation
             // look at each individual character in the str 
             // and increment the count if that char is uppercase
 
-           // foreach (char  in password)
-           // { 
-            //    char.IsUpper
-            //}
+            foreach (char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++;
+                }
+            }
             // Return the number of uppercase characters.
             return upperCase;
         }
-
         // The NumberLowerCase method accepts a string argument
         // and returns the number of lowercase letters it contains.
         private int NumberLowerCase(string str)
@@ -41,44 +43,57 @@ namespace Password_Validation
             int lowerCase = 0;  // The number of lowercase letters
 
             // Count the lowercase characters in str.
-
-
-            // Return the number of lowercase characters.
-            return lowerCase;
-        }
-
-        // The NumberDigits method accepts a string argument
-        // and returns the number of numeric digits it contains.
-        private int NumberDigits(string str)
-        {
-            int digits = 0;  // The number of digits
-
-            // Count the digits in str.
-
-
-            // Return the number of digits.
-            return digits;
-        }
-
-        private void checkPasswordButton_Click(object sender, EventArgs e)
-        {
-            const int MIN_LENGTH = 8;  // Password's minimum length
-
-            // Get the password from the TextBox.
-            string password = passwordTextBox.Text;
-
-            // Validate the password.
-            if (NumberUpperCase(password) >= 1 && NumberLowerCase(password) >= 1
-                    && NumberDigits(password) >= 8)
-            {
-                MessageBox.Show("The password is valid.");
+                foreach (char ch in str)
+                {
+                    if (char.IsLower(ch))
+                    {
+                        lowerCase++;
+                    }
+                }
+                // Return the number of lowercase characters.
+                return lowerCase;
             }
-            else
+
+            // The NumberDigits method accepts a string argument
+            // and returns the number of numeric digits it contains.
+            private int NumberDigits(string str)
             {
-                MessageBox.Show("The password does not meet " +
-                    "the requirements.");
+                int digits = 0;  // The number of digits
+
+                // Count the digits in str.
+                foreach (char ch in str)
+                {
+
+                    if (char.IsDigit(ch))
+                    {
+                        digits++;
+                    }
+                }
+                // Return the number of digits.
+                return digits;
             }
-        }
+
+            private void checkPasswordButton_Click(object sender, EventArgs e)
+            {
+                const int MIN_LENGTH = 8;  // Password's minimum length
+
+                // Get the password from the TextBox.
+                string password = passwordTextBox.Text;
+
+                // Validate the password.
+                if (password.Length >= MIN_LENGTH &&
+                    NumberUpperCase(password) >= 1 &&
+                    NumberLowerCase(password) >= 1 &&
+                    NumberDigits(password) >= 1)
+                {
+                    MessageBox.Show("The password is valid.");
+                }
+                else
+                {
+                    MessageBox.Show("The password does not meet " +
+                        "the requirements.");
+                }
+            }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
